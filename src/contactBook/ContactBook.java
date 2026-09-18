bf4b801f019d3191644dccc629597abec005fdef
mmpamaro71274 <mmp.amaro@campus.fct.unl.pt>
2026-09-18 22:38:53 +0100

    Deleted the words "contactBook." from the tests files, Tests and Main. Completed the function getContact() from ContactBook.

--- src/contactBook/ContactBook.java
+++ src/contactBook/ContactBook.java
@@ -91,4 +91,13 @@ public class ContactBook {
         return contacts[currentContact++];
     }
 
+    //Search for a contact based on its phone number
+    public String getContact(int phone) {
+        for (int i = 0; i < counter; i++) {
+            if (contacts[i].getPhone() == phone) {
+                return contacts[i].getName();
+            }
+        }
+        return null;
+    }
 }
